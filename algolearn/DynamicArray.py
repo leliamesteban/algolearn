fd63f0305196e0418dd11bc2c8167ba39ab20964
Liam Esteban Prince <liamesteban@gmail.com>
2024-08-28 14:34:18 +0200

    Create DynamicArray.py

---
+++ algolearn/DynamicArray.py
@@ -0,0 +1,29 @@
+class DynamicArray:
+  """
+
+  """
+  def __init__(self):
+    self._capacity = 1
+    self._size = 0
+    self._elements = [None] * self._capacity
+
+  def resize(self, size):
+    pass
+
+  def append(self, element):
+    pass
+  
+  def getItem(self, index):
+    pass
+
+  def setItem(self, index, value):
+    pass
+
+  def delItem(self, index):
+    pass
+
+  def len(self):
+    return self._size
+  
+  def toString(self):
+    pass
