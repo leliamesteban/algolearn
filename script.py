123de7c1ee56ab3cd1f1975c48d5299709a557c6
Liam Esteban Prince <liamesteban@gmail.com>
2024-08-28 10:21:39 +0200

    Create object

--- script.py
+++ script.py
@@ -1,13 +1,29 @@
-def multiply(number, multiplier):
+class Multiplication:
   """
-  Multiply a given number by a given multiplier.
+  Instantiate a multiplication operation.
+  Numbers will be multiplied by the given multiplier.
 
-  :param number: The number to multiply.
-  :type number:
-
-  :param multiplier: The multiplier.
+  :param multiplier:
   :type multiplier: int
   """
-  return number * multiplier
+  def __init__(self, multiplier):
+    self.multiplier = multiplier
+  
+  def multiply(number: int, multiplier: int) -> int:
+    """
+    Multiply a given number by a given multiplier.
+  
+    :param number: The number to multiply.
+    :type number:
+  
+    :param multiplier: The multiplier.
+    :type multiplier: int
 
-print(multiply(5, 2)
+    :return: The result of the multiplication
+    :rtype: int
+    """
+    return number * multiplier
+
+multiplication = Multiplication(2)
+
+print(multiplication.multiply(5))
