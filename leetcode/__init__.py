92fb077094d5985aff67d993d8c660fa6c008e58
Liam Esteban Prince <liamesteban@gmail.com>
2024-08-28 12:41:56 +0200

    Update __init__.py

--- leetcode/__init__.py
+++ leetcode/__init__.py
@@ -1,2 +1,2 @@
 # Import the class in __init__.py so that the module can be imported with "from leetcode import Multiplication"
-from leetcode.multiplication import Multiplication
+from algolearn.multiplication import Multiplication
